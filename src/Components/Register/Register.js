b103da1097c362eda4df0f8e08e1117cbb338179
arunsnd2017 <arunsinghsnd@gmail.com>
2020-06-15 11:48:46 +0530

    adding comment

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Register = ({onRouteChange}) =>
 {
   return(
-
+// here useage is tachyons for degsining
   <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
     <main className="pa4 black-80">
   <div className="measure">
